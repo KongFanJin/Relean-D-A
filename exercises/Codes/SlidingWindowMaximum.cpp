fbc9514910eea8cbdf38d370a60e5386ecbc3855
刘宁 <549613278@qq.com>
2020-10-23 04:55:19 +0800

    Update SlidingWindowMaximum.cpp

--- exercises/Codes/SlidingWindowMaximum.cpp
+++ exercises/Codes/SlidingWindowMaximum.cpp
@@ -16,19 +16,3 @@ public:
         return ans;
     }
 };
-
-class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        if(nums.size() == 0) return {};
-        vector<int> ans;
-        for(int i = 0; i <= nums.size() - k; i ++){
-            int maxNum = nums[i];
-            for(int j = i; j < i + k; j ++){
-                maxNum = max(maxNum, nums[j]);
-            }
-            ans.push_back(maxNum);
-        }
-        return ans;
-    }
-};
