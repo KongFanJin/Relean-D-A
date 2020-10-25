2df6b81677da169823d5b8d530e7b0946b788c88
KongFanJin <59203947+KongFanJin@users.noreply.github.com>
2020-10-25 19:05:22 +0800

    重构代码

--- exercises/SlidingWindowMaximum/Codes/Huahua/BSTMultiset.cpp
+++ exercises/SlidingWindowMaximum/Codes/Huahua/BSTMultiset.cpp
@@ -1,25 +1,15 @@
 // Author: Huahua
 
-// https://docs.microsoft.com/zh-cn/cpp/standard-library/multiset-class?view=vs-2019
 /*
-   The C++ Standard Library multiset class is used for the storage and retrieval of data from a collection 
-   in which the values of the elements contained need not be unique and in which they serve as the key values 
-   according to which the data is automatically ordered. The key value of an element in a multiset may not be changed directly. 
-   Instead, old values must be deleted and elements with new values inserted.
-*/
+  multiset https://zh.cppreference.com/w/cpp/container/multiset
 
-/*
   multiset::rbegin()
   Returns a const iterator addressing the first element in a reversed multiset.
   
   multiset::equal_range()
-  Returns a pair of iterators. 
-  The first iterator in the pair points to the first element in a multiset with a key that is greater than a specified key. 
-  The second iterator in the pair points to the first element in the multiset with a key that is equal to or greater than the key.
-  
-   If no match is found for the key, both elements of the pair return end( )
-
-  就是返回: first <= e <= second
+  含一对定义所需范围的迭代器的 std::pair ：第一个指向首个不小于 key 的元素，第二个指向首个大于 key 的元素。
+  若无元素不小于 key ，则将尾后（见 end() ）迭代器作为第一元素返回。
+  类似地，若无元素大于 key ，则将尾后迭代器作为第二元素返回。
 
   multiset::void erase( iterator pos )
   删除迭代器指向的元素
@@ -28,22 +18,35 @@
 // 通过 1284 ms 162.7 MB C++
 // 时间复杂度分析:红黑树排序O(nlogn)
 
-class Solution {
-   public:
-    vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+class Solution
+{
+public:
+    vector<int> maxSlidingWindow(vector<int> &nums, int k)
+    {
         vector<int> ans;
 
-        if (nums.empty()) return ans;
+        if (nums.empty())
+            return ans;
 
-        // 将[0,0+k-1)的k个元素加入到multiset中
-        // 创建完集合后,集合的大小固定了,必须删除元素才能插入元素
-        multiset<int> window(nums.begin(), nums.begin() + k-1); 
-        for (int i = k - 1; i < nums.size(); ++i) {
-            window.insert(nums[i]); // 插入
-            ans.push_back(*window.rbegin()); // multiset默认递增序列;获得最大值,存入ans
+        // 将[0,0+k-1)的k-1个元素加入到multiset中
+        multiset<int> window(nums.begin(), nums.begin() + k - 1);
+        for (int i = k - 1; i < nums.size(); ++i)
+        {
+            window.insert(nums[i]); // 插入,此时窗口有k个元素
 
-            if (i - k + 1 >= 0) //窗口有k个元素时
-                window.erase(window.equal_range(nums[i - k + 1]).first); // 删除比nums[i - k + 1]的元素
+            // if (i - k + 1 >= 0) { //窗口有k个元素时,考虑找出窗口的最大值
+
+            // multiset默认递增序列;获得最大值,存入ans
+            ans.push_back(*window.rbegin());
+
+            // 创建完集合后,集合的大小固定了,必须删除元素才能插入元素
+            // (0,...,i-k,[i-(k-1),...,i-1,i],i+1,...,n-1) 要向右滑动窗口
+            // nums[i-k+1]是窗口位置的第1个元素,需要被移除窗口
+            // 10 - 8 = 2 : ...,8,9,10,... ;i - (i-k+1) = k - 1
+
+            // window.erase(window.equal_range(nums[i - k + 1]).first);
+            window.erase(window.find(nums[i - (k - 1)]));
+            // }
         }
 
         return ans;
